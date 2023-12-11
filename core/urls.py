2f9c1493eb4d11f7fbbf7616dd31103fa7d0f7bd
Evaletik <104141950+Evaletik@users.noreply.github.com>
2023-12-11 22:08:02 +0100

    feat: some new views

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-
+    path("", HomeView.as_view(), name = 'home'),
+    path("groups", GroupListView.as_view(), name = 'group_list'),
+    path("groups/<int:pk>/", GroupDetailView.as_view(), name = 'group_detail'),
+    path("groups/<int:pk>/lessons", GroupLessonsListView.as_view(), name = 'group_lessons'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,33 @@
 from typing import Any
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import *
 
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+
 class GroupLessonsListView(ListView):
     model = Lesson
     template_name = "core/group_lessons_list.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
+        context['lessons'] = Lesson.objects.filter(group__id=self.kwargs["pk"])
+        return context
+
+
+class GroupListView(ListView):
+    model = Group
+    template_name = "core/group_list.html"
+
+
+class GroupDetailView(DetailView):
+    model = Group
+    template_name = "core/group_detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["students"] = Student.objects.filter(group__id=self.kwargs["pk"])
         return context
