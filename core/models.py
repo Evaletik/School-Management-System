d0811d7726f5b27b3baa6e35c3792121181e1507
h1r0kuu <abhazim.nurbat@gmail.com>
2023-12-12 20:57:13 +0100

    feat: add verbose name and __str__ methods for some models

--- core/models.py
+++ core/models.py
@@ -4,8 +4,24 @@ from django.db import models
 class Subject(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Subject"
+        verbose_name_plural = "Subjects"
+
+
 class Group(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Subject"
+        verbose_name_plural = "Subjects"
+
 
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
@@ -15,24 +31,54 @@ class Teacher(models.Model):
     birthDate = models.DateField()
     birthPlace = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True,)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True, )
     password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.firstName} {self.lastName}'
+
+    class Meta:
+        verbose_name = "Teacher"
+        verbose_name_plural = "Teachers"
+
 
 class Lesson(models.Model):
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
     teacher = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
     subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.teacher.__str__()} {self.subject.name}'
+
+    class Meta:
+        verbose_name = "Lesson"
+        verbose_name_plural = "Lessons"
+
+
 class Assignment(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, null=True)
     due_date = models.DateTimeField()
     description = models.CharField(max_length=20)
     title = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name = "Assignment"
+        verbose_name_plural = "Assignments"
+
+
 class Mark(models.Model):
     id = models.AutoField(primary_key=True)
     mark = models.DecimalField(max_digits=3, decimal_places=2)
     student = models.ForeignKey('Student', on_delete=models.SET_NULL, null=True)
     assignment = models.ForeignKey('Assignment', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.student.id} - {self.mark}'
+
+    class Meta:
+        verbose_name = "Mark"
+        verbose_name_plural = "Marks"
+
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
@@ -41,10 +87,18 @@ class Student(models.Model):
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
     birthPlace = models.CharField(max_length=255)
     phone = models.CharField(max_length=20)
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True,)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True, )
     password = models.CharField(max_length=100)
     birthDate = models.DateField()
     createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.firstName} {self.lastName}'
+
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+
 
 class Attendance(models.Model):
     STATUS_CHOICES = [
@@ -55,3 +109,10 @@ class Attendance(models.Model):
     student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True)
     assignment = models.ForeignKey(Assignment, on_delete=models.SET_NULL, null=True)
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
+
+    def __str__(self):
+        return f'{self.student.id} {self.status}'
+
+    class Meta:
+        verbose_name = "Attendance"
+        verbose_name_plural = "Attendances"
