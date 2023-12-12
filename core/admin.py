6a5f8397434404c41dc555b9a06d59d6ea7843e3
h1r0kuu <abhazim.nurbat@gmail.com>
2023-12-12 20:57:43 +0100

    feat: beautify admin panel with pagination, filtering and search field

--- core/admin.py
+++ core/admin.py
@@ -5,27 +5,37 @@ from .models import *
 # Register your models here.
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_per_page = 10
 
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ("name",)
+    list_display = ("name",)
+    list_per_page = 10
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ("group", "teacher", "subject")
+    list_display = ("group", "teacher", "subject")
+    list_per_page = 10
 
 
 @admin.register(Assignment)
 class AssignmentAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ("lesson", "due_date", "description", "title")
+    list_display = ("lesson", "due_date", "description", "title")
+    list_per_page = 10
 
 
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ("student", "assignment", "status")
+    list_display = ("student", "assignment", "status")
+    list_per_page = 10
 
 
 @admin.register(Student)
