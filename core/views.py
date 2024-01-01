42a4baf5c510c71a096ff5439064c31187c11fdb
h1r0kuu <abhazim.nurbat@gmail.com>
2024-01-01 19:50:20 +0100

    feat: group attendances list views and schedule view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from typing import Any
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import *
@@ -8,13 +9,23 @@ class HomeView(TemplateView):
     template_name = "index.html"
 
 
-class GroupLessonsListView(ListView):
+class GroupLessonsListView(LoginRequiredMixin, ListView):
     model = Lesson
-    template_name = "core/group_lessons_list.html"
+    template_name = "core/student/my_schedule.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['assignments'] = Assignment.objects.filter(lesson__group__id=self.kwargs["pk"])
+        return context
+
+
+class AttendancesListView(LoginRequiredMixin, ListView):
+    model = Attendance
+    template_name = "core/teacher/attendance.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['attendances'] = Attendance.objects.filter(student__group__id=self.kwargs["pk"])
         return context
 
 
@@ -30,4 +41,15 @@ class GroupDetailView(DetailView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["students"] = Student.objects.filter(group__id=self.kwargs["pk"])
+        return context
+
+
+class MySchedule(LoginRequiredMixin, ListView):
+    model = Assignment
+    template_name = "core/student/my_schedule.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        user_group: Group = Student.objects.get(id=self.request.user.id).group
+        context['assignments'] = Assignment.objects.filter(lesson__group__id=user_group.id)
         return context
