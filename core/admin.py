12d13a48c3eb2ceb764a4e58c1ba822a81b60cee
h1r0kuu <abhazim.nurbat@gmail.com>
2024-01-01 19:49:28 +0100

    refactor: update assignment dates

--- core/admin.py
+++ core/admin.py
@@ -28,8 +28,8 @@ class LessonAdmin(admin.ModelAdmin):
 
 @admin.register(Assignment)
 class AssignmentAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    search_fields = ("lesson", "due_date", "description", "title")
-    list_display = ("lesson", "due_date", "description", "title")
+    search_fields = ("lesson", "from_date", "to_date", "description", "title")
+    list_display = ("lesson", "from_date", "to_date", "description", "title")
     list_per_page = 10
     resource_class = AssignmentResource
 
