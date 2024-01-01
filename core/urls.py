3cf0559468581638aa01c716131a98f66cf94b0a
h1r0kuu <abhazim.nurbat@gmail.com>
2024-01-01 19:49:43 +0100

    feat: add group attendances and user schedule

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("groups", GroupListView.as_view(), name='group_list'),
     path("groups/<int:pk>/", GroupDetailView.as_view(), name='group_detail'),
     path("groups/<int:pk>/lessons", GroupLessonsListView.as_view(), name='group_lessons'),
-    # path("schedule",),
-    # path("grades"),
+    path("groups/<int:pk>/attendances", AttendancesListView.as_view(), name='group_attendances'),
+    path("my_schedule", MySchedule.as_view(), name='my_schedule')
+    # path("teacher/<int:pk>/schedule", MySchedule.as_view(), name='my_schedule')
+    # path("group/<int:pk>/schedule", MySchedule.as_view(), name='my_schedule')
 ]
