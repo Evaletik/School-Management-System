e9645b5b9b945279b215c768fd1a210dad3691b3
h1r0kuu <abhazim.nurbat@gmail.com>
2024-01-02 23:39:56 +0100

    feat: some new views

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,15 @@ from .views import *
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
     path("groups", GroupListView.as_view(), name='group_list'),
+
+
     path("groups/<int:pk>/", GroupDetailView.as_view(), name='group_detail'),
     path("groups/<int:pk>/lessons", GroupLessonsListView.as_view(), name='group_lessons'),
-    path("groups/<int:pk>/attendances", AttendancesListView.as_view(), name='group_attendances'),
-    path("my_schedule", MySchedule.as_view(), name='my_schedule')
-    # path("teacher/<int:pk>/schedule", MySchedule.as_view(), name='my_schedule')
-    # path("group/<int:pk>/schedule", MySchedule.as_view(), name='my_schedule')
+    path("groups/<int:pk>/attendances", GroupAttendancesListView.as_view(), name='group_attendances'),
+    path("assignments", Assignments.as_view(), name='assignments'),
+    path("assignments/<int:pk>/set_attendances", SetAttendance.as_view(), name='set_attendances'),
+
+    path("my_schedule", MySchedule.as_view(), name='my_schedule'),
+    path("teacher/<int:pk>/schedule", TeacherScheduleListView.as_view(), name='teacher_schedule'),
+    path("group/<int:pk>/schedule", GroupScheduleListView.as_view(), name='group_schedule'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from typing import Any
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import View, ListView, DetailView, TemplateView
 from .models import *
+
+
+# from django.views.generic.edit import CreateView
 
 
 # Create your views here.
@@ -19,7 +23,7 @@ class GroupLessonsListView(LoginRequiredMixin, ListView):
         return context
 
 
-class AttendancesListView(LoginRequiredMixin, ListView):
+class GroupAttendancesListView(LoginRequiredMixin, ListView):
     model = Attendance
     template_name = "core/teacher/attendance.html"
 
@@ -44,6 +48,46 @@ class GroupDetailView(DetailView):
         return context
 
 
+class Assignments(ListView):
+    model = Assignment
+    template_name = "core/teacher/assignments.html"
+    context_object_name = 'assignments'
+
+    def get_queryset(self):
+        teacher = Teacher.objects.get(id=self.request.user.id)
+        assignments = Assignment.objects.filter(lesson__teacher=teacher)
+
+        for assignment in assignments:
+            assignment.present_students_count = assignment.attendance_set.filter(status='P').count()
+
+        return assignments
+
+
+class SetAttendance(View):
+    # model = Attendance
+    # fields = ['status']
+    template_name = "core/teacher/set_attendances.html"
+
+    def get(self, request, pk):
+        assignment = get_object_or_404(Assignment, id=pk)
+        students = Student.objects.filter(group=assignment.lesson.group)
+        return render(request, self.template_name, {'assignment': assignment, 'students': students})
+
+    def post(self, request, pk):
+        assignment = get_object_or_404(Assignment, id=pk)
+        students = Student.objects.filter(group=assignment.lesson.group)
+        new_attendances = [
+            Attendance(student=student, assignment=assignment,
+                       status=request.POST.get(f'student_{student.id}_attendance', 'A'))
+            for student in students
+            if not assignment.attendance_set.filter(student__id=student.id).count() > 1
+        ]
+
+        Attendance.objects.bulk_create(new_attendances)
+
+        return redirect(to="group_attendances", pk=assignment.lesson.group.id)
+
+
 class MySchedule(LoginRequiredMixin, ListView):
     model = Assignment
     template_name = "core/student/my_schedule.html"
@@ -53,3 +97,23 @@ class MySchedule(LoginRequiredMixin, ListView):
         user_group: Group = Student.objects.get(id=self.request.user.id).group
         context['assignments'] = Assignment.objects.filter(lesson__group__id=user_group.id)
         return context
+
+
+class TeacherScheduleListView(ListView):
+    model = Assignment
+    template_name = "core/teacher/schedule.html"
+    context_object_name = 'assignments'
+
+    def get_queryset(self):
+        teacher = Teacher.objects.values('id').get(id=self.kwargs['pk'])
+        return Assignment.objects.filter(lesson__teacher=teacher['id'])
+
+
+class GroupScheduleListView(ListView):
+    model = Assignment
+    template_name = "core/group/schedule.html"
+    context_object_name = 'assignments'
+
+    def get_queryset(self):
+        group = Group.objects.values('id').get(id=self.kwargs['pk'])
+        return Assignment.objects.filter(lesson__group=group['id'])
